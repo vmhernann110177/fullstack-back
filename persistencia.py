f356bf03361f18b811279c16b3071aecdff9cca1
Victor Hernández <65037576+vmhernann110177@users.noreply.github.com>
2021-12-10 19:15:50 -0600

    Create persistencia.py

---
+++ persistencia.py
@@ -0,0 +1,7 @@
+
+def guardar_pedido(nombre, apellidos):
+    with open("pedidos.txt", "a", encoding="utf-8") as file:
+        file.write(nombre + " " + apellidos + "\n")
+        file.close()
+
+guardar_pedido("Victor_", "Hernández_")
